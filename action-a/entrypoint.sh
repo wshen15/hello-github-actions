6af5881b45cb9fd5cd3ca3eb043d07e6729646e8
wshen15 <70672038+wshen15@users.noreply.github.com>
2020-11-11 20:05:12 -0600

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
+if($INPUT_MY_NAME<0)
+  sh -c "echo Negative number"
+else if($INPUT_MY_NAME<0)
+  sh -c "echo Zero"
+else
+  sh -c "echo Positive number"
